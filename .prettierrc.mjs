9912085b0f1572de0ef96370cbd101eb7edb52da
fo <103907142+Foparty@users.noreply.github.com>
2025-04-01 13:56:48 +0100

    done

---
+++ .prettierrc.mjs
@@ -0,0 +1,13 @@
+tsco;
+/** @type {import("prettier").Config} */
+export default {
+  plugins: ["prettier-plugin-astro"],
+  overrides: [
+    {
+      files: "*.astro",
+      options: {
+        parser: "astro",
+      },
+    },
+  ],
+};
